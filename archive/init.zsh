f68a30a5b2073a6ea766ab49b7ac68ff208b745f
meh <meh@schizofreni.co>
2012-10-06 14:52:15 +0200

    archive: add internal plugin

---
+++ archive/init.zsh
@@ -0,0 +1,5 @@
+zdefault ':zoppo:plugin:archive:extract' extensions \
+  '7z' 'Z' 'bz2' 'deb' 'gz' 'lzma' 'rar' 'tar' 'tar.bz2' 'tar.gz' 'tar.xz' \
+  'tar.zma' 'tbz' 'tbz2' 'tgz' 'tlz' 'txz' 'xz' 'zip'
+
+# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
