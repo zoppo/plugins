284c60a055ab8d7b5c16a7aea72323aec89ea4c5
meh <meh@schizofreni.co>
2012-10-08 00:41:01 +0200

    utility: add GNU coreutils support

--- utility/init.zsh
+++ utility/init.zsh
@@ -1,3 +1,45 @@
+if zstyle -t ':zoppo:plugin:utility:gnu' enable; then
+  function {
+    local prefix
+    local -a binaries
+    local cmd
+
+    zdefault -s ':zoppo:plugin:utility:gnu' prefix prefix 'g'
+    zdefault -a ':zoppo:plugin:utility:gnu' binaries binaries \
+      '[' 'base64' 'basename' 'cat' 'chcon' 'chgrp' 'chmod' 'chown' \
+      'chroot' 'cksum' 'comm' 'cp' 'csplit' 'cut' 'date' 'dd' 'df' \
+      'dir' 'dircolors' 'dirname' 'du' 'echo' 'env' 'expand' 'expr' \
+      'factor' 'false' 'fmt' 'fold' 'groups' 'head' 'hostid' 'id' \
+      'install' 'join' 'kill' 'link' 'ln' 'logname' 'ls' 'md5sum' \
+      'mkdir' 'mkfifo' 'mknod' 'mktemp' 'mv' 'nice' 'nl' 'nohup' 'nproc' \
+      'od' 'paste' 'pathchk' 'pinee' 'pr' 'printenv' 'printf' 'ptx' \
+      'pwd' 'readlink' 'realpath' 'rm' 'rmdir' 'runcon' 'seq' 'sha1sum' \
+      'sha224sum' 'sha256sum' 'sha384sum' 'sha512sum' 'shred' 'shuf' \
+      'sleep' 'sort' 'split' 'stat' 'stty' 'sum' 'sync' 'tac' 'tail' \
+      'tee' 'test' 'timeout' 'touch' 'tr' 'true' 'truncate' 'tsort' \
+      'tty' 'uname' 'unexpand' 'uniq' 'unlink' 'uptime' 'users' 'vdir' \
+      'wc' 'who' 'whoami' 'yes' \
+      \
+      'addr2line' 'ar' 'c++filt' 'elfedit' 'nm' 'objcopy' 'objdump' \
+      'ranlib' 'readelf' 'size' 'strings' 'strip' \
+      \
+      'find' 'locate' 'oldfind' 'updatedb' 'xargs' \
+      \
+      'libtool' 'libtoolize' \
+      \
+      'getopt' 'grep' 'indent' 'sed' 'tar' 'time' 'units' 'which'
+
+    for cmd in "$binaries[@]"; do
+      is-callable "$prefix$cmd" && continue
+      is-callable "$cmd" || continue
+
+      eval "function $cmd {
+        $prefix$cmd \"$@\"
+      }"
+    done
+  }
+fi
+
 if zstyle -t ':zoppo:plugin:utility:ls' color; then
   if ! zstyle -T ':zoppo:plugin:utility:ls' colors; then
     zstyle -s ':zoppo:plugin:utility:ls' colors LS_COLORS
