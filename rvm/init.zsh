0d3dc7f66365a0611b7dc804fa4d73177172092e
meh <meh@schizofreni.co>
2012-10-09 16:50:26 +0200

    rvm: remove it, it's merged in ruby now

--- rvm/init.zsh
+++
@@ -1,7 +0,0 @@
-zdefault -s ':zoppo:plugin:rvm' path zoppo_rvm_path "${HOME}/.rvm/scripts/rvm"
-
-[[ -s "$zoppo_rvm_path" ]] && source "$zoppo_rvm_path"
-
-unset zoppo_rvm_path
-
-# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
