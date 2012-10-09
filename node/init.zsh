295e2eddbe99d7606f8fe6f88f94029b9767cfdd
meh <meh@schizofreni.co>
2012-10-09 17:26:12 +0200

    node: make npm enableable by zstyle

--- node/init.zsh
+++ node/init.zsh
@@ -1,13 +1,16 @@
-zdefault -s ':zoppo:plugin:node:npm' cache cache "$(path:cache)/npm.zsh"
+if zdefault -t ':zoppo:plugin:node:npm' enable 'yes'; then
+  function {
+    local cache
+    zdefault -s ':zoppo:plugin:node:npm' cache cache "$(path:cache)/npm.zsh"
 
-if is-callable npm; then
-  if [[ "$commands[npm]" -nt "$cache" || ! -s "$cache" ]]; then
-    npm completion >! "$cache" &> /dev/null
-  fi
+    if is-callable npm; then
+      if [[ "$commands[npm]" -nt "$cache" || ! -s "$cache" ]]; then
+        npm completion >! "$cache" &> /dev/null
+      fi
 
-  source "$cache"
+      source "$cache"
+    fi
+  }
 fi
-
-unset cache
 
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
