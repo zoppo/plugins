34b3992b7e08cbfd1b4a5b94b644ad82c050da58
meh <meh@schizofreni.co>
2012-10-09 15:41:18 +0200

    haskell: add internal plugin

---
+++ haskell/init.zsh
@@ -0,0 +1,10 @@
+# prepend Cabal per user directories to PATH/MANPATH
+if [[ "$OSTYPE" == darwin* ]]; then
+  path=($HOME/Library/Haskell/bin(/N) $path)
+  manpath=($HOME/Library/Haskell/man(/N) $manpath)
+else
+  path=($HOME/.cabal/bin(/N) $path)
+  manpath=($HOME/.cabal/man(/N) $manpath)
+fi
+
+# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
