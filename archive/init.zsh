1dfb7596de4591e182a73acd9fc6b3c235f25414
meh <meh@schizofreni.co>
2012-10-13 18:08:09 +0200

    archive: refactor code

--- archive/init.zsh
+++ archive/init.zsh
@@ -1,5 +1,6 @@
-zdefault ':zoppo:plugin:archive:extract' extensions \
-  '7z' 'Z' 'bz2' 'deb' 'gz' 'lzma' 'rar' 'tar' 'tar.bz2' 'tar.gz' 'tar.xz' \
-  'tar.zma' 'tbz' 'tbz2' 'tgz' 'tlz' 'txz' 'xz' 'zip'
+zdefault ':zoppo:plugin:archive' disable 'deb'
+
+alias extract=archive:extract
+alias ls-archive=archive:list
 
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
