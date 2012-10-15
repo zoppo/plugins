a8edecb0314a8eac9433811c1a8693b26dc0bab6
meh <meh@schizofreni.co>
2012-10-15 18:07:34 +0200

    command-not-found: add plugin for not found commands

---
+++ command-not-found/init.zsh
@@ -0,0 +1,13 @@
+if zdefault -t ':zoppo:plugin:command-not-found' no-correct 'no'; then
+  unsetopt CORRECT CORRECT_ALL
+fi
+
+if [[ -s '/etc/zsh_command_not_found' ]]; then
+  source '/etc/zsh_command_not_found'
+elif [[ -s '/usr/share/doc/pkgfile/command-not-found.zsh' ]]; then
+  source '/usr/share/doc/pkgfile/command-not-found.zsh'
+else
+  return 1
+fi
+
+# vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
