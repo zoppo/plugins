37c551e57e62f2d8fd00f4aac410a254a6249133
meh <meh@schizofreni.co>
2012-10-15 15:33:32 +0200

    haskell: properly handle extended globs

--- haskell/init.zsh
+++ haskell/init.zsh
@@ -1,10 +1,16 @@
-# prepend Cabal per user directories to PATH/MANPATH
-if [[ "$OSTYPE" == darwin* ]]; then
-  path=($HOME/Library/Haskell/bin(/N) $path)
-  manpath=($HOME/Library/Haskell/man(/N) $manpath)
-else
-  path=($HOME/.cabal/bin(/N) $path)
-  manpath=($HOME/.cabal/man(/N) $manpath)
-fi
+function {
+  typeset -gU path manpath
+
+  setopt LOCAL_OPTIONS EXTENDED_GLOB
+
+  # prepend Cabal per user directories to PATH/MANPATH
+  if [[ "$OSTYPE" == darwin* ]]; then
+    path=($HOME/Library/Haskell/bin(/N) $path)
+    manpath=($HOME/Library/Haskell/man(/N) $manpath)
+  else
+    path=($HOME/.cabal/bin(/N) $path)
+    manpath=($HOME/.cabal/man(/N) $manpath)
+  fi
+}
 
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
