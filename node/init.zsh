28fbbc0bff1e4dd9c781e7493a16b3b868924d6f
shurizzle <me@shurizzle.dev>
2023-07-02 16:15:35 +0200

    node: keep NODE_PATH env var updated

--- node/init.zsh
+++ node/init.zsh
@@ -9,10 +9,27 @@ if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
 
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+    local prefix="$(npm config get prefix)"
+
     if [ "$(nvm current)" = system ] && is-command npm; then
       typeset -gU path
-      path=("$(npm config get prefix)"/bin(/N) $path)
+      path=("$prefix"/bin(/N) $path)
     fi
+
+    typeset -g NODE_PATH
+    NODE_PATH="${prefix}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+    export NODE_PATH
+
+    eval "nvm() {
+${functions[nvm]}
+typeset -g NODE_PATH
+NODE_PATH=\"\$(nvm_strip_path \"\${NODE_PATH}\" /lib/node_modules)\"
+hash -r
+if is-command npm; then
+  NODE_PATH=\"\$(npm config get prefix)/lib/node_modules\${NODE_PATH:+:\$NODE_PATH}\"
+fi
+export NODE_PATH
+}"
 
     if plugins:load-if-enabled 'completion'; then
       [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
@@ -20,10 +37,16 @@ if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
   }
 elif zdefault -t ':zoppo:plugin:node:npm' enable 'yes'; then
   function {
+    local prefix="$(npm config get prefix)"
+
     if is-command npm; then
       typeset -gU path
-      path=("$(npm config get prefix)"/bin(/N) $path)
+      path=("$prefix"/bin(/N) $path)
     fi
+
+    typeset -g NODE_PATH
+    NODE_PATH="${prefix}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+    export NODE_PATH
   }
 fi
 # }}}
