0d2d4ed91c2b4611f52c53e82f20e7219e92557e
shurizzle <shura1991@gmail.com>
2023-07-02 17:01:22 +0200

    node: improve nvm detection

--- node/init.zsh
+++ node/init.zsh
@@ -3,46 +3,60 @@ if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
   function {
     typeset -g NVM_DIR
     if [ -z "${NVM_DIR-}" ]; then
-      zdefault -s ':zoppo:plugin:node:nvm' path NVM_DIR "$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+      NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    fi
+    if [ -z "${NVM_DIR-}" ]; then
+      zdefault -s ':zoppo:plugin:node:nvm' path NVM_DIR "${NVM_DIR}"
     fi
     export NVM_DIR
 
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+    if [ -s "$NVM_DIR/nvm.sh" ]; then 
+      . "$NVM_DIR/nvm.sh"
 
-    local prefix="$(npm config get prefix)"
+      if zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
+        local prefix="$(command npm config get prefix)"
 
-    if [ "$(nvm current)" = system ] && is-command npm; then
+        if [ "$(nvm current)" = system ]; then
+          typeset -gU path
+          path=("$prefix"/bin(/N) $path)
+        fi
+
+        typeset -g NODE_PATH
+        NODE_PATH="${prefix}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+        export NODE_PATH
+
+        eval "nvm() {
+  ${functions[nvm]}
+  typeset -g NODE_PATH
+  NODE_PATH=\"\$(nvm_strip_path \"\${NODE_PATH}\" /lib/node_modules)\"
+  hash -r
+  if is-command npm; then
+    NODE_PATH=\"\$(command npm config get prefix)/lib/node_modules\${NODE_PATH:+:\$NODE_PATH}\"
+  fi
+  export NODE_PATH
+  }"
+      fi
+    elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
+      local prefix="$(command npm config get prefix)"
+
       typeset -gU path
       path=("$prefix"/bin(/N) $path)
+
+      typeset -g NODE_PATH
+      NODE_PATH="${prefix}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+      export NODE_PATH
     fi
-
-    typeset -g NODE_PATH
-    NODE_PATH="${prefix}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
-    export NODE_PATH
-
-    eval "nvm() {
-${functions[nvm]}
-typeset -g NODE_PATH
-NODE_PATH=\"\$(nvm_strip_path \"\${NODE_PATH}\" /lib/node_modules)\"
-hash -r
-if is-command npm; then
-  NODE_PATH=\"\$(npm config get prefix)/lib/node_modules\${NODE_PATH:+:\$NODE_PATH}\"
-fi
-export NODE_PATH
-}"
 
     if plugins:load-if-enabled 'completion'; then
       [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
     fi
   }
-elif zdefault -t ':zoppo:plugin:node:npm' enable 'yes'; then
+elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
   function {
-    local prefix="$(npm config get prefix)"
+    local prefix="$(command npm config get prefix)"
 
-    if is-command npm; then
-      typeset -gU path
-      path=("$prefix"/bin(/N) $path)
-    fi
+    typeset -gU path
+    path=("$prefix"/bin(/N) $path)
 
     typeset -g NODE_PATH
     NODE_PATH="${prefix}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
