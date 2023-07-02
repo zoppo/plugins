6b0dfa014189cfbdee6bf07c0e54a83e88c6b565
shurizzle <me@shurizzle.dev>
2023-07-02 13:40:13 +0200

    Add nvm support to node plugin

--- node/README.md
+++ node/README.md
@@ -9,9 +9,21 @@ npm Completion
 --------------
 You can enable/disable npm completion.
 
-The default is enabled'
+The default is enabled.
 
     zstyle ':zoppo:plugin:node:npm' enabled 'no'
+
+nvm Integration
+---------------
+You can enable/disable nvm integration.
+
+The default is enabled.
+
+    zstyle ':zoppo:plugin:node:nvm' enabled 'yes'
+
+You can change `$NVM_DIR` if you don't like the default one.
+
+    zstyle ':zoppo:plugin:node:nvm' path "$HOME/.nvm"
 
 [1]: http://nodejs.org
 [2]: http://npmjs.org
--- node/init.zsh
+++ node/init.zsh
@@ -15,4 +15,22 @@ if zdefault -t ':zoppo:plugin:node:npm' enable 'yes'; then
 fi
 # }}}
 
+# NVM {{{
+if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
+  function {
+    typeset -g NVM_DIR
+    if [ -z "${NVM_DIR-}" ]; then
+      zdefault -s ':zoppo:plugin:node:nvm' path NVM_DIR "$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    fi
+    export NVM_DIR
+
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+    if plugins:load-if-enabled 'completion'; then
+      [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+    fi
+  }
+fi
+# }}}
+
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
