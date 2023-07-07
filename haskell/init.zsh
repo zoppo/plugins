a2e3006c2ec9beb54494bbb3810ef5c9deb18d0e
shurizzle <me@shurizzle.dev>
2023-07-08 00:35:52 +0200

    Add ghcup support to haskell plugin

--- haskell/init.zsh
+++ haskell/init.zsh
@@ -1,3 +1,32 @@
+# ghcup Support {{{
+if zdefault -t ':zoppo:plugin:haskell:ghcup' enable 'no'; then
+  function {
+    typeset -gU path
+    typeset -g GHCUP_USE_XDG_DIRS GHCUP_INSTALL_BASE_PREFIX
+
+    setopt LOCAL_OPTIONS EXTENDED_GLOB BARE_GLOB_QUAL
+
+    if zdefault -t ':zoppo:plugin:haskell:ghcup' xdg 'yes'; then
+      GHCUP_USE_XDG_DIRS=true
+      export GHCUP_USE_XDG_DIRS
+      unset GHCUP_INSTALL_BASE_PREFIX
+
+      path=("${XDG_BIN_HOME:-"${HOME}/.local/bin"}"(/N) $path)
+    else
+      unset GHCUP_USE_XDG_DIRS
+
+      GHCUP_INSTALL_BASE_PREFIX="${GHCUP_INSTALL_BASE_PREFIX:-"${HOME}"}"
+      zdefault -s ':zoppo:plugin:haskell:ghcup' home \
+        GHCUP_INSTALL_BASE_PREFIX "${GHCUP_INSTALL_BASE_PREFIX}"
+      export GHCUP_INSTALL_BASE_PREFIX
+
+      path=("${GHCUP_INSTALL_BASE_PREFIX}/.ghcup/bin"(/N) $path)
+    fi
+
+  }
+fi
+# }}}
+
 # Path Settings {{{
 function {
   typeset -gU path manpath
