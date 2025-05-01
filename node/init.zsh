4eeca6757845ba43ed85eea96d98c2725912be06
shurizzle <me@shurizzle.dev>
2025-05-01 07:31:58 +0200

    feat(node): autoload the specified node version in .nvmrc

--- node/init.zsh
+++ node/init.zsh
@@ -51,6 +51,26 @@ if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
       [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
     fi
   }
+  autoload -U add-zsh-hook
+  load-nvmrc() {
+    local node_version="$(nvm version)"
+    local nvmrc_path="$(nvm_find_nvmrc)"
+
+    if [ -n "$nvmrc_path" ]; then
+      local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+      if [ "$nvmrc_node_version" = "N/A" ]; then
+        nvm install
+      elif [ "$nvmrc_node_version" != "$node_version" ]; then
+        nvm use
+      fi
+    elif [ "$node_version" != "$(nvm version default)" ]; then
+      echo "Reverting to nvm default version"
+      nvm use default
+    fi
+  }
+  add-zsh-hook chpwd load-nvmrc
+  load-nvmrc
 elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
   function {
     local prefix="$(command npm config get prefix)"
