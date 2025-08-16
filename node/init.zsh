9d238169c784893e78e836916163a502611df80c
shurizzle <me@shurizzle.dev>
2025-08-16 16:17:57 +0200

    fix: run nvm even when `default` alias doesn't exist

--- node/init.zsh
+++ node/init.zsh
@@ -18,20 +18,20 @@ if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
     fi
   }
 
-  function {
-    typeset -g NVM_DIR
-    if [ -z "${NVM_DIR-}" ]; then
-      NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    fi
-    if [ -z "${NVM_DIR-}" ]; then
-      zdefault -s ':zoppo:plugin:node:nvm' path NVM_DIR "${NVM_DIR}"
-    fi
-    export NVM_DIR
+  typeset -g NVM_DIR
+  if [ -z "${NVM_DIR-}" ]; then
+    NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  fi
+  if [ -z "${NVM_DIR-}" ]; then
+    zdefault -s ':zoppo:plugin:node:nvm' path NVM_DIR "${NVM_DIR}"
+  fi
+  export NVM_DIR
 
-    if [ -s "$NVM_DIR/nvm.sh" ]; then 
-      . "$NVM_DIR/nvm.sh"
+  if [ -s "$NVM_DIR/nvm.sh" ]; then 
+    . "$NVM_DIR/nvm.sh"
 
-      if zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
+    if zdefault -t ':zoppo:plugin:node:modules' enable 'yes'; then
+      if is-command npm; then
         if [ "$(nvm current)" = system ]; then
           typeset -gU path
           path=("$prefix"/bin(/N) $path)
@@ -40,44 +40,46 @@ if zdefault -t ':zoppo:plugin:node:nvm' enable 'no'; then
         typeset -g NODE_PATH
         NODE_PATH="$(cd ~ && command npm config get prefix)/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
         export NODE_PATH
-
-        eval "nvm() {
-  ${functions[nvm]}
-  typeset -g NODE_PATH
-  NODE_PATH=\"\$(nvm_strip_path \"\${NODE_PATH}\" /lib/node_modules)\"
-  hash -r
-  if is-command npm; then
-    NODE_PATH=\"\$(functions[nvm:loadrc]=''; cd ~ && command npm config get prefix)/lib/node_modules\${NODE_PATH:+:\$NODE_PATH}\"
-  fi
-  export NODE_PATH
-  }"
-
-        autoload -U add-zsh-hook
-        add-zsh-hook chpwd nvm:loadrc
-        nvm:loadrc
+      else
+        typeset -g NODE_PATH
+        NODE_PATH=
+        export NODE_PATH
       fi
-    elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
-      typeset -gU path
-      path=("$prefix"/bin(/N) $path)
 
-      typeset -g NODE_PATH
-      NODE_PATH="$(cd ~ && command npm config get prefix)/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
-      export NODE_PATH
-    fi
+      eval "nvm() {
+${functions[nvm]}
+typeset -g NODE_PATH
+NODE_PATH=\"\$(nvm_strip_path \"\${NODE_PATH}\" /lib/node_modules)\"
+hash -r
+if is-command npm; then
+  NODE_PATH=\"\$(functions[nvm:loadrc]=''; cd ~ && command npm config get prefix)/lib/node_modules\${NODE_PATH:+:\$NODE_PATH}\"
+fi
+export NODE_PATH
+}"
 
-    if plugins:load-if-enabled 'completion'; then
-      [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+      autoload -U add-zsh-hook
+      add-zsh-hook chpwd nvm:loadrc
+      nvm:loadrc
     fi
-  }
-elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
-  function {
+  elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
     typeset -gU path
     path=("$prefix"/bin(/N) $path)
 
     typeset -g NODE_PATH
     NODE_PATH="$(cd ~ && command npm config get prefix)/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
     export NODE_PATH
-  }
+  fi
+
+  if plugins:load-if-enabled 'completion'; then
+    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+  fi
+elif zdefault -t ':zoppo:plugin:node:modules' enable 'yes' && is-command npm; then
+  typeset -gU path
+  path=("$prefix"/bin(/N) $path)
+
+  typeset -g NODE_PATH
+  NODE_PATH="$(cd ~ && command npm config get prefix)/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+  export NODE_PATH
 fi
 # }}}
 
@@ -99,22 +101,20 @@ fi
 # }}}
 
 if zdefault -t ':zoppo:plugin:node:modules' enable 'yes'; then
-  function {
-    typeset -g NPM_PACKAGES
-    zdefault -s ':zoppo:plugin:node:modules' path NPM_PACKAGES "${HOME}/.node_modules"
-    export NPM_PACKAGES
+  typeset -g NPM_PACKAGES
+  zdefault -s ':zoppo:plugin:node:modules' path NPM_PACKAGES "${HOME}/.node_modules"
+  export NPM_PACKAGES
 
-    if ! [ -d "${NPM_PACKAGES}/bin" ]; then
-      mkdir -p "${NPM_PACKAGES}/bin"
-    fi
+  if ! [ -d "${NPM_PACKAGES}/bin" ]; then
+    mkdir -p "${NPM_PACKAGES}/bin"
+  fi
 
-    typeset -gU path
-    path=("${NPM_PACKAGES}"/bin(/N) $path)
+  typeset -gU path
+  path=("${NPM_PACKAGES}"/bin(/N) $path)
 
-    typeset -g NODE_PATH
-    NODE_PATH="${NPM_PACKAGES}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
-    export NODE_PATH
-  }
+  typeset -g NODE_PATH
+  NODE_PATH="${NPM_PACKAGES}/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+  export NODE_PATH
 fi
 
 # vim: ft=zsh sts=2 ts=2 sw=2 et fdm=marker fmr={{{,}}}
